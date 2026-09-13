761851c010981cfacb6c375475f985b0c36418e9
Harshita445 <harshitak0456@gmail.com>
2026-09-13 11:17:07 +0530

    Updates

---
+++ _next/static/nAJ2WkhyjbeuppOrJp5Xg/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fhandover\u002F[id]","\u002Fprofile\u002F[id]","\u002Fprojects\u002F[id]","\u002Fprojects\u002F[id]\u002Fdiscussions","\u002Frequest-stewardship\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
